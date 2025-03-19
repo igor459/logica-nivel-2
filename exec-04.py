1ff6c71ac0a611ea86bd3973d6f7242f30bece7a
igor459 <igorsanta20@gmail.com>
2025-03-19 20:33:52 -0300

    Create exec-04.py

---
+++ exec-04.py
@@ -0,0 +1,7 @@
+# Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+print("diga um numero")
+numero1 = int(input())
+n2 =1 
+resultado = (numero1 - n2)
+print("seu antecessor é")
+print(numero1 - n2)
