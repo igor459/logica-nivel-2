74531028e7b0df2f2f53495fe2930d068f85b00d
igor459 <igorsanta20@gmail.com>
2025-03-19 22:01:42 -0300

    Create exec-06.py

---
+++ exec-06.py
@@ -0,0 +1,9 @@
+ #Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias
+print("qual sua idade em anos:")
+n1=int(input())
+print("qual sua idade em meses:")
+n2=int(input())
+n3 = 365
+n4 = 30
+print("sua idade em dias é:")
+print(n1*n3+n2*n4)
